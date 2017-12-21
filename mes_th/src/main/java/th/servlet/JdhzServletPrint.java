d3b171e6ae84be527d4b90299d7f0fc4fd294200
Ajaxfan <ajax_2003@163.com>
2017-12-21 23:39:47 +0800

    解决了重复打印的问题

--- mes_th/src/main/java/th/servlet/JdhzServletPrint.java
+++ mes_th/src/main/java/th/servlet/JdhzServletPrint.java
@@ -16,63 +16,64 @@ import com.qm.mes.th.assembly.newprint.AssemblyNewPrintFacade;
  * @author Administrator
  */
 public class JdhzServletPrint extends HttpServlet {
-    /** 序列号 */
-    private static final long serialVersionUID = 1L;
+	/** 序列号 */
+	private static final long serialVersionUID = 1L;
 
-    /**
-     * 数据处理
-     */
-    public void service(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        // 从客户端接收到的请求参数
-        RequestParam requestParam = new RequestParam();
-        requestParam.setRequestDate(request.getParameter("rq"));// 请求日期
-        requestParam.setChassisNumber(request.getParameter("ch"));// 底盘号
-        requestParam.setGroupId(request.getParameter("groupid"));// 打印组号
-        requestParam.setJs(request.getParameter("js"));// 打印批次号
+	/**
+	 * 数据处理
+	 */
+	public  synchronized void service(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		// 从客户端接收到的请求参数
+		RequestParam requestParam = new RequestParam();
+		requestParam.setRequestDate(request.getParameter("rq"));// 请求日期
+		requestParam.setChassisNumber(request.getParameter("ch"));// 底盘号
+		requestParam.setGroupId(request.getParameter("groupid"));// 打印组号
+		requestParam.setJs(request.getParameter("js"));// 打印批次号
 
-        // 数据写出到客户端
-        write2Client(response, new AssemblyNewPrintFacade().assemblyPrint(requestParam));
-    }
+		// 数据写出到客户端
+		write2Client(response, new AssemblyNewPrintFacade().assemblyPrint(requestParam));
+	}
 
-    /**
-     * 数据写出到客户端
-     * 
-     * @param response
-     * @param obj
-     */
-    private void write2Client(HttpServletResponse response, Object obj) {
-        // 设置响应头
-        response.setContentType("application/octet-stream");
-        
-        OutputStream streamOutPut = null;
-        ObjectOutputStream objectOutput = null;
+	/**
+	 * 数据写出到客户端
+	 * 
+	 * @param response
+	 * @param obj
+	 */
+	private void write2Client(HttpServletResponse response, Object obj) {
+		// 设置响应头
+		response.setContentType("application/octet-stream");
 
-        try {
-            streamOutPut = response.getOutputStream();
-            objectOutput = new ObjectOutputStream(streamOutPut);
+		OutputStream streamOutPut = null;
+		ObjectOutputStream objectOutput = null;
 
-            objectOutput.writeObject(obj);
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (objectOutput != null) {
-                try {
-                    objectOutput.close();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                } finally {
-                    objectOutput = null;
-                }
-            }
-            if (streamOutPut != null) {
-                try {
-                    streamOutPut.close();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                } finally {
-                    streamOutPut = null;
-                }
-            }
-        }
-    }
+		try {
+			streamOutPut = response.getOutputStream();
+			objectOutput = new ObjectOutputStream(streamOutPut);
+
+			objectOutput.writeObject(obj);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (objectOutput != null) {
+				try {
+					objectOutput.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				} finally {
+					objectOutput = null;
+				}
+			}
+			if (streamOutPut != null) {
+				try {
+					streamOutPut.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				} finally {
+					streamOutPut = null;
+				}
+			}
+		}
+	}
 }
